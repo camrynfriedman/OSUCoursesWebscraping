3937ad40fcbf7977d2deb702a45374d08b01ff4c
MysticMatt <mysticmatt99@gmail.com>
2022-02-20 00:52:48 -0500

    Modified the storage of classes, for easy use with front-end text interface

--- scraper2.rb
+++ scraper2.rb
@@ -1,78 +1,100 @@
 require 'mechanize'
 require 'json'
 
+class Course
+    attr_reader :title, :subCat, :description, :teachers, :maxCH, :minCH
+    def initialize(title, subCat, description, teachers, maxCH, minCH)
+        @title = title
+        @subCat = subCat
+        @description = description
+        @teachers = teachers
+        @maxCH = maxCH
+        @minCH = minCH
+    end
+end
+
 class Scraper
 	# An scraper object that has 
-	attr_accessor :url, :courses, :terms, :title, :subject_catalogNumber
+	attr_accessor :url, :coursePages, :courseCatalog
 
 	# Created, immplemented, tested on 02/29/2022 by Yifan Zhou
 	def initialize
 		@url = "https://content.osu.edu/v2/classes/search"
+        @initial_query = "?q=cse&campus=col&term=1224&p=1"
         # An hashtable <<int>page_num: <hashTable>courses_data>, stores all the data about course
-        @courses = {}
-        
-        # Collecting some useful information about courses
-        @terms = []
-        @title = []
-        @subject_catalogNumber = []
+        @coursePages = {}
+
+        @courseCatalog = []
         # menu_html = Nokogiri::HTML(menu['panels'][0]['html'])
         # course_content = json_data["data"]['courses'][0]["course"]["term"]
 	end
 
+    #Should be unnecessary now but just in case we need it
     # Taking page_num as input, and store the result in courses
-    def store_courses_page(page_num)
-        agent = Mechanize.new       # Instantiate a Mechanize object
-        html_data = agent.get(uri=@url, query={"q": "cse", "campus": "col_mrn_nwk_lma", "term": "1224", "p": page_num})
-        # Parse to ruby interpretable data structure
-        json_data = JSON.parse html_data.body
-        # Save to courses instance
-        @courses[page_num] = json_data["data"]['courses']
-        course_content = json_data["data"]['courses'][0]["course"]["term"]
-        puts "Course: #{course_content}"
-    end
+    #def store_courses_page(page_num)
+        #agent = Mechanize.new       # Instantiate a Mechanize object
+        #html_data = agent.get(uri=@url, query={"q": "cse", "campus": "col", "term": "1224", "p": page_num})
+        ## Parse to ruby interpretable data structure
+        #json_data = JSON.parse html_data.body
+        ## Save to courses instance
+        #@coursePages[page_num] = json_data["data"]['courses']
+    #end
 
     # Taking page_num as input, and store the result in courses
     def store_all_courses_page()
         puts "===> Start scraping all courses info..."
-        agent = Mechanize.new       # Instantiate a Mechanize object
         page_num = 1
-        html_data = agent.get(uri=@url, query={"q": "cse", "campus": "col_mrn_nwk_lma", "term": "1224", "p": page_num})
+        agent = Mechanize.new       # Instantiate a Mechanize object
+        puts "Initial page request: " + @url + @initial_query
+        html_data = agent.get(@url + @initial_query)
+        
         # rescue Mechanize::ResponseReadError => e
         json_data = JSON.parse html_data.body
         # Get total number of pages
         totalPages = json_data["data"]["totalPages"]
-        puts "Total pages: #{totalPages}"
         # Store current page data
-        @courses[page_num] = json_data["data"]['courses']
-        # Test the result
-        puts json_data["data"]['courses'][0]["course"]["term"]
+        @coursePages[page_num] = json_data["data"]['courses']
 
         while json_data["data"]["nextPageLink"] != nil
-            html_data = agent.get @url + json_data["data"]["nextPageLink"]
+            page_num += 1
+            sleep(2)
+            puts "Getting Page #{page_num}: " + @url + json_data["data"]["nextPageLink"]
+            html_data = agent.get(@url + json_data["data"]["nextPageLink"])
             json_data = JSON.parse html_data.body
-            @courses[page_num] = json_data["data"]['courses']
-            puts json_data["data"]['courses'][0]["course"]["term"]
+            @coursePages[page_num] = json_data["data"]['courses']
         end
 
-        File.write('./course-data.json', JSON.dump(@courses))
         puts "===> Finished scraping!"
+        puts "===> Total pages: #{totalPages}"
+        return totalPages
     end
 
     # Collecting the useful information about each courses in page_num
     def get_course_info(page_num)
-        @courses[page_num].each do |item| # for each things in things do something while storing that things in the variable item
+        @coursePages[page_num].each do |item| # for each things in things do something while storing that things in the variable item
             # print "#{item}"
-            @terms << item["course"]["term"]
-            @title << item["course"]["title"]
-            @subject_catalogNumber << item["course"]["subject"] + item["course"]["catalogNumber"]
+            newTitle = item["course"]["title"]
+            newSubCat = item["course"]["subject"] + item["course"]["catalogNumber"]
+            newDesc = item["course"]["description"]
+            newTeachArray = []
+            newMaxCH = item["course"]["maxUnits"]
+            newMinCH = item["course"]["minUnits"]
+
+            item["sections"].each do |section|
+                if !(newTeachArray.include? section["meetings"][0]["instructors"][0]["displayName"]) && (section["meetings"][0]["instructors"][0]["displayName"] != nil)
+                    newTeachArray << section["meetings"][0]["instructors"][0]["displayName"]
+                end
+            end 
+
+            @courseCatalog << Course.new(newTitle, newSubCat, newDesc, newTeachArray, newMaxCH, newMinCH)
         end
     end
 
     def pretty_print(page_num)
 		puts "A exaustive list of courses in #{page_num}!"
-		@terms.each_with_index do |item, index|
+		@courseCatalog.each_with_index do |item, index|
 			# puts name.class
-			puts "#{index}; Course Term: #{item}; Course Number: #{@subject_catalogNumber[index]}; Course Title: #{@title[index]}"
+			puts "#{index}; Course Number: #{item.subCat}; Course Title: #{item.title}"
 		end
 	end 
 
@@ -80,14 +102,35 @@ end
 
 # Some example how to use it!
 scraper = Scraper.new
-scraper.store_all_courses_page()
-# file = File.read('./course-data.json')
-# scraper.courses = JSON.parse(file)
-# scraper.store_courses_page(1)
-# scraper.get_course_info(2)
-# puts scraper.terms[0]   # Expected ==> Summer 2022
-# puts scraper.title[0]   # Expected ==> Modeling and Problem Solving with Spreadsheets and Databases
-# puts scraper.subject_catalogNumber[0]   # Expected ==> CSE2111
-# scraper.pretty_print(2)
+totalPages = scraper.store_all_courses_page()
+File.write('.course-data.json', JSON.dump(scraper.coursePages))
 
-# File.write('./course-data.json', JSON.dump(data_hash))
+#file = File.read('./course-data.json')
+#scraper.coursePages = JSON.parse(file)
+
+
+for i in 1..totalPages
+    scraper.get_course_info(i)
+end
+
+
+scraper.courseCatalog.each do |item|
+    puts item.subCat
+    puts item.title
+    puts item.description
+    print "Instructors: #{item.teachers}\n"
+    print item.minCH
+    if item.minCH != item.maxCH
+        print "-#{item.maxCH}"
+    end
+    puts
+    puts
+end
+
+
+
+
+#scraper.pretty_print(4)
+
+#File.write('./finalFile.json', JSON.dump(scraper.coursePages))
+#puts "write finished\n\n\n"
