0f382b8c2fa6eec1a00af0f47c1effac71d93ede
Bencp2 <Pearlstein.14@osu.edu>
2022-02-21 11:41:10 -0500

    edited search.rb

--- search.rb
+++ search.rb
@@ -24,24 +24,65 @@ for i in 1..totalPages
     scraper.get_course_info(i)
 end
 
+courseNum, courseName, descriptor, instrName, creditHrs = nil, nil, nil, nil, nil
+continue = true
+while (continue)
 #welcome message
 puts "\nWelcome to the CSE course search!"
 puts "You can search by course number, course name, description, instructor name, and course credit hours"
-puts "Enter 1 for the course number (i.e. 'MATH2568')"
-puts "Enter 2 for the course name (i.e. 'Linear Algebra')"
-puts "Enter 3 to search for a word in the description (i.e. 'Prereq')" #can use the include? method for strings here if x.description.include? "prereq"
-puts "Enter 4 to search by instructor"
-puts "Enter 5 to search for course credit hours"
+puts "Enter 1 for course number (i.e. 'MATH2568')"
+puts "Enter 2 for course name (i.e. 'Linear Algebra')"
+puts "Enter 3 for a word in the description (i.e. 'Prereq')" #can use the include? method for strings here if x.description.include? "prereq"
+puts "Enter 4 for instructor"
+puts "Enter 5 for course credit hours"
+puts "Enter 6 to get the results"
 
-puts "\nPlease enter a field to search by: "
+print "\nPlease enter a field to search by or 6 for results: "
 fieldName = gets.to_i
 
-while !fieldName.between?(1,5)
-    puts "\nPlease enter a valid field number"
+while !fieldName.between?(1,6)
+    print "\nPlease enter a valid option: "
     fieldName = gets.to_i
 end
+if (fieldName == 1)
+    print "\nPlease enter what course you would like to search for by providing the name and number with no spaces (i.e. MATH2568): "
+    user_input = gets.upcase
+elsif (fieldName == 2)
+    print "\nPlease enter what course title you would like to search for (i.e. 'Linear Algebra'): "
+    user_input = gets.chomp.upcase
+elsif (fieldName == 3)
+    print "\nPlease enter a keyword(s) from the course description (i.e. 'Prereq'): "
+    user_input = gets.chomp.upcase
+elsif (fieldName == 4)
+    print "\nPlease enter the instructor's full name: "
+    user_input = gets.chomp
+elsif (fieldName == 5)
+    print "\nPlease enter the number of credit hours you are looking for: "
+    user_input = gets.to_i
+elsif (fieldName == 6)
+continue = false
+end
+end
 
-count = 0 #to keep track of how many results show up after the search. also helpful for printing error message
+if (courseNum != nil)
+searchCourseNum(courseNum)
+end
+if (courseName != nil)
+searchCourseName(courseName)
+end
+if (descriptor != nil)
+searchDescription(descriptor)
+end
+if (instrName != nil)
+searchInstructor(instrName)
+end
+if (creditHrs != nil)
+searchCredit(creditHrs)
+end
+
+count = courseResults.length #to keep track of how many results show up after the search. also helpful for printing error message
+
+
 
 #course number
 if fieldName == 1
